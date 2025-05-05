f800ddc4ea5786d4aefe7647b7434802d88bf1f2
miguel-abulencia <miguel.abulencia@bizdata.com.au>
2025-05-05 10:51:21 +0800

    local build for fuzzy search

---
+++ build/precache-manifest.c5310a54251eec5da54a7e213a3ac8bb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8070b656ed52cc840e237e02cb2a3d13",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3596fa9a1dcdb8121485",
+    "url": "/static/css/2.7ab99566.chunk.css"
+  },
+  {
+    "revision": "4b231dfe35c33f79f3de",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "3596fa9a1dcdb8121485",
+    "url": "/static/js/2.c6cedc07.chunk.js"
+  },
+  {
+    "revision": "bb949995c75bda98c3b16110f59c5f48",
+    "url": "/static/js/2.c6cedc07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17752a61ddd85b5ce172",
+    "url": "/static/js/3.81387d9c.chunk.js"
+  },
+  {
+    "revision": "4b231dfe35c33f79f3de",
+    "url": "/static/js/main.aac8f8b5.chunk.js"
+  },
+  {
+    "revision": "b55f6e833b7630f04dbe",
+    "url": "/static/js/runtime-main.a3e3de89.js"
+  }
+]);
